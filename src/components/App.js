a92d0278cb6ae1ecb39d91214e49c6f5cad1e4a0
Pablo-Es <szyposz@gmail.com>
2018-02-25 12:38:25 +0100

    create menu component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Menu from './Menu';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
