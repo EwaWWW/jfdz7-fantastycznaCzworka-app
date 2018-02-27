7f66d8a519807354c75ad71dad01a4cfa17018a8
auraya33 <kasia.nagel@gmail.com>
2018-02-27 12:32:27 +0100

    add Search Filter component

---
+++ src/components/SearchView/SearchFilter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class SearchFilter extends Component {
+  render() {
+    return (
+        <button class="ui toggle button">${}</button>
+    )
+  }
+}
+
+export default SearchFilter
