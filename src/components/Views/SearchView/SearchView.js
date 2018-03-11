8eaccbfd970206e540492ea7ddbee1fe9079eea4
JustynaBed <justyna.bednarko@gmail.com>
2018-03-11 12:04:11 +0100

    change location components of table with WishesView

--- src/components/Views/SearchView/SearchView.js
+++ src/components/Views/SearchView/SearchView.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import SearchFilter from "./SearchFilter";
-
+import WishesView from "../WishesView/WishesView";
 
 class SearchView extends Component {
     render() {
         return (
             <React.Fragment>
                 <SearchFilter/>
+                <WishesView/>
             </React.Fragment>
         )
     }
