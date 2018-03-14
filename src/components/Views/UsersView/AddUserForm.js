8a3bb8ac8519269c9cda3735669336d3adb12cf9
Pablo-Es <szyposz@gmail.com>
2018-03-14 15:31:00 +0100

    create add user form file

---
+++ src/components/Views/UsersView/AddUserForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+class AddUserForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default AddUserForm
