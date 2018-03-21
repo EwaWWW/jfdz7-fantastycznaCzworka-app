c447052e62760b6d946d43660ee8124c3815b5e5
auraya33 <kasia.nagel@gmail.com>
2018-03-21 21:19:37 +0100

    change firebase project

--- src/setupFirebase.js
+++ src/setupFirebase.js
@@ -1,12 +1,11 @@
 import firebase from 'firebase'
 
-
 const config = {
-    apiKey: "AIzaSyBWUamb9kD4u6mAlAFMKjh5lXXJ2tJQm6U",
-    authDomain: "fantastycznaczworka-app.firebaseapp.com",
-    databaseURL: "https://fantastycznaczworka-app.firebaseio.com",
-    projectId: "fantastycznaczworka-app",
+    apiKey: "AIzaSyB2AWzs9kwxcceSWkASP-GBWYZCN4_vp6s",
+    authDomain: "fc-generator-zyczen.firebaseapp.com",
+    databaseURL: "https://fc-generator-zyczen.firebaseio.com",
+    projectId: "fc-generator-zyczen",
     storageBucket: "",
-    messagingSenderId: "655206000760"
+    messagingSenderId: "299749350476"
 };
 firebase.initializeApp(config);
