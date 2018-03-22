2f347059436896285175fe82ffdf70450ff772b3
EwaWWW <ewawozniak@vp.pl>
2018-03-22 02:47:59 +0100

    filter for text - Input

--- src/components/Views/SearchView/SearchView.js
+++ src/components/Views/SearchView/SearchView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Table, Grid, Button } from 'semantic-ui-react'
+import { Table, Grid, Button} from 'semantic-ui-react'
+import {Search}from 'semantic-ui-react'
+
 
 import SearchFilter from "./SearchFilter"
 import TableRowWish from "../../Views/WishesView/TableRowWish"
@@ -10,7 +12,8 @@ class SearchView extends Component {
 
     state = {
         allWishes: wishes,
-        category: ''
+        category: '',
+        searchValue: ''
     };
 
 
@@ -19,15 +22,35 @@ class SearchView extends Component {
             category: category
         },() => {console.log(this.state)}
         )
-    }
+    };
+
+    handleSearch = (e) => {
+        this.setState({
+            searchValue: (e).target.value
+        })
+    };
+
 
 render() {
 
-    const wishes = this.state.category ? this.state.allWishes.filter(wish => wish.category === this.state.category)
+    const wishes = this.state.category ? this.state.allWishes.filter(wish =>
+        wish.category === this.state.category)
          :
         this.state.allWishes;
+
         return (
             <React.Fragment>
+                <Search onChange={this.handleSearch} onselectionchange={this.state.allWishes.filter(
+                    wish => ( this.state.searchValue === '' ? true : [
+                            wish.wish,
+                            wishes.category
+                        ].map(
+                            wish => wish.toLowerCase()
+                        ).some(
+                            searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
+                            ))
+                    )
+                )}>Wyszukaj życzenie</Search>
                 <Grid centered padded>
                       <SearchFilter
                              filterToggle={this.updateCategory}/>
@@ -40,14 +63,28 @@ render() {
                             <Table.HeaderCell width={1}><b>Dodaj do ulubionych</b></Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
-                    <Table.Body>
-                {wishes.map(wish =>
-                    <TableRowWish
-                        id={wish.id}
-                        wish={wish.wish}
-                        category={wish.category}/>
-                )}
-                    </Table.Body>
+                    <Table.Body>{
+                                wishes.map(wish =>
+                            <TableRowWish
+                                id={wish.id}
+                                wish={wish.wish}
+                                category={wish.category}/>
+                        )}
+                        /*{
+     // //  this.state.allWishes.filter(
+    // //           wish => (
+    // //         this.state.searchValue === '' ? true : [
+    // //             wish.wish,
+    // //             wishes.category
+    // //         ].map(
+    // //             wish => wish.toLowerCase()
+    // //         ).some(
+    // //             searchValue => searchValue.includes(this.state.searchValue.toLowerCase()
+    // //             ))
+    // //     )
+    // // )
+                    }*/
+                        </Table.Body>
                 </Table>
                 <Button color='red' onClick={() =>
                     this.refs.addWish.openModal()
