7a988010acb26753393d43e68cb7a21fe50dd50b
JustynaBed <justyna.bednarko@gmail.com>
2018-04-01 21:01:09 +0200

    create reducers for favorite wishes

--- src/state/wishes.js
+++ src/state/wishes.js
@@ -1,16 +1,38 @@
 import wishesFromFile from '../Data/wishes';
 
-const initialState = { wishes: wishesFromFile }
+
+const initialState = { wishes: wishesFromFile, favorites: [] }
 
 export const ADD_WISH = "ADD_WISH"
+export const ADD_FAVORITE = "ADD_FAVORITE"
+export const REMOVE_FAVORITE = "REMOVE_FAVORITE"
+
+
 export const addWish = (data) => ({
     type: ADD_WISH, data
 })
+
+export const addFavorite = (data) => ({
+    type: ADD_FAVORITE, data
+})
+
+export const removeFavorite = (data) => ({
+    type: REMOVE_FAVORITE, data
+})
+
 export default function(state = initialState, action) {
     switch (action.type) {
         case ADD_WISH:
             console.log("AddWish", state, action, state.wishes)
             return { ...state, wishes: [...state.wishes, action.data]}
+        case ADD_FAVORITE:
+            return {
+
+            }
+        case REMOVE_FAVORITE:
+            return {
+
+            }
         default:
             console.log("default", state)
             return state
